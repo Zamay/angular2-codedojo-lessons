175d29a9c216ae04252bf50cb332044f06f2125f
zaamy <mityazamay@gmail.com>
2017-05-31 11:07:46 +0300

    2й урок

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,4 +1,9 @@
-/**
- * Created by zamaj on 31.05.2017.
- */
+import {NgModel} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
 
+@NgModele({
+    inports: [BrowserModule]
+})
+export class AppModule{
+
+}
