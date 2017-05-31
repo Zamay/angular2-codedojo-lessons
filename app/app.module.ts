f7d2d1fad05b221854264764004d9ac031e0b804
zaamy <mityazamay@gmail.com>
2017-05-31 12:09:52 +0300

    3 - Создание компонента

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,11 @@
-import {NgModel} from "@angular/forms";
+import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "./app.component";
 
-@NgModele({
-    inports: [BrowserModule]
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule{
 
